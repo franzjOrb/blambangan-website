4f95e53aea1ab7f12d07010aab80ac11c39ac914
Joost Gunawan <joostchristian42@gmail.com>
2026-08-09 20:11:04 +0700

    #feat(footer): add copyright to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer className="footer">
             <span className="footer-text">
-                © 2026 CV. Blambangan Advertising. All rights reserved.
+                &copy; {new Date().getFullYear()} CV. Blambangan Advertising. All rights reserved.
             </span>
             <span className="footer-text">
                 Jl. S. Limboto 52 A/9, Makassar — (0411) 314185 
