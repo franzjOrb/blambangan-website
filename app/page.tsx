284c596291ccc44fe06fddab7b3b8fb4ef2edff5
Joost Gunawan <joostchristian42@gmail.com>
2026-08-11 20:19:00 +0700

    #chore(page): remove unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useEffect, useState } from 'react';
-import Image from "next/image";
 import Navbar from '@/components/Navbar';
 import HeroSection from '@/components/HeroSection';
 import ServicesSection from '@/components/ServicesSection';
