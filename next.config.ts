1bcf393612d94a73b2d1afdb6df4aa7a22232d79
Joost Gunawan <joostchristian42@gmail.com>
2026-08-11 21:17:15 +0700

    #chore(config): remove config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
